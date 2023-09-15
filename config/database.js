f8030ff695d86929542a893dd6f24f75d89fed03
Nookesh <nookeshkarri7@gmail.com>
2023-09-15 08:21:20 +0530

    up[dated

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "mysql",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "157.245.102.21"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "rrcenxpwqk"),
+      user: env("DATABASE_USERNAME", "rrcenxpwqk"),
+      password: env("DATABASE_PASSWORD", "6YyseexNnw"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
